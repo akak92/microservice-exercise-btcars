5ecd89555421ce1f9993f1c809e2c3aafde97a09
Pedro <pfdiaz@smn.gob.ar>
2024-05-24 11:51:05 -0300

    Modifico pruebas unitarias

--- Services/api-service/api/tests/test_btcars.py
+++ Services/api-service/api/tests/test_btcars.py
@@ -1,7 +1,11 @@
 #
 #   Pedro Díaz | 23-05-2023
-#   Test unitarios para funcion GET_BTCars  
+#   Test unitarios para controlador BTCars 
 #   
+
+#
+# Pruebas unitarias para la obtención de precio bitcoin
+#
 
 def test_btcars_GET_BTCars_success(app, client):
     response = client.get('/btcars/1716517049')
@@ -21,3 +25,66 @@ def test_btcars_GET_BTCars_invalid_timestamp(app, client):
     response = client.get('/btcars/invalid')
     assert response.status_code == 400
 
+#
+# Pruebas unitarias para el cálculo de promedio
+#
+
+def test_btcars_promedio_success(app, client):
+    response = client.get('/btcars/promedio?init=1716519438&end=1716522306')
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data['message'] == 'Promedio obtenido exitosamente.'
+    assert 'prom' in json_data
+
+def test_btcars_promedio_no_docs(app, client):
+    response = client.get('/btcars/promedio?init=1716519438&end=1716522306')
+    assert response.status_code == 404
+    json_data = response.get_json()
+    assert json_data['message'] == 'No existen documentos para el rango solicitado.'
+    assert json_data['prom'] is None
+
+def test_btcars_promedio_invalid_range(app, client):
+    response = client.get('/btcars/promedio?init=1716522306&end=1716519438')  # init > end
+    assert response.status_code == 400
+    json_data = response.get_json()
+    assert json_data['message'] == 'Búsqueda mal formada. end debe ser mayor que init.'
+    assert json_data['prom'] is None
+
+def test_btcars_promedio_missing_params(app, client):
+    response = client.get('/btcars/promedio')
+    assert response.status_code == 400
+    json_data = response.get_json()
+    assert json_data['message'] == 'Consulta mal formada. Se requieren parametros init Y end'
+    assert json_data['prom'] is None
+
+#
+# Pruebas unitarias para función de paginación
+#
+
+def test_btcars_pagination_with_timestamps_success(app, client):
+    response = client.get('/btcars?page=1&init=1716519438&end=1716522306')
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data['message'] == 'Documentos obtenidos con filtro de timestamp.'
+    assert 'elements' in json_data
+
+def test_btcars_pagination_without_timestamps_success(app, client):
+    response = client.get('/btcars?page=1')
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data['message'] == 'Documentos obtenidos sin filtro de timestamp.'
+    assert 'elements' in json_data
+
+def test_btcars_pagination_invalid_page(app, client):
+    response = client.get('/btcars?page=-1&init=1716519438&end=1716522306')
+    assert response.status_code == 400
+    json_data = response.get_json()
+    assert json_data['message'] == 'La página no puede ser menor que 1'
+    assert json_data['elements'] is None
+
+def test_btcars_pagination_missing_page(app, client):
+    response = client.get('/btcars?init=1716519438&end=1716522306')
+    assert response.status_code == 400
+    json_data = response.get_json()
+    assert json_data['message'] == 'Se necesita parámetro page para paginación'
+    assert json_data['elements'] is None
